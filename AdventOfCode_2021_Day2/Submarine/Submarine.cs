bac137fdd71133f4c195017e877d017a6bdd2fa2
acheatham87 <acheatham87@gmail.com>
2022-07-29 13:48:23 -0500

    I think / hope this is correct

--- AdventOfCode_2021_Day2/Submarine/Submarine.cs
+++ AdventOfCode_2021_Day2/Submarine/Submarine.cs
@@ -1,13 +1,62 @@
-﻿namespace Submarine {
-    public class Submarine : ISubmarine {
+﻿
+
+
+namespace Submarine {
+    public class Submarine : ISubmarine 
+    {
+        public int depth = 0;
+        public int horizontalPosition = 0;
 
         public Submarine(string InstructionFile = "input.txt") : this(File.ReadAllLines(InstructionFile)) { }
 
         public Submarine(string[] Instructions) {
-            
+            foreach (string[] instruction in Instructions)
+            {
+                instruction.Split(" ");
+                string direction = instruction[1];
+                int value = int.Parse(instruction[2]);
+
+                if(direction == "forward")
+                {
+                    forward(value);
+                }
+                if (direction == "up")
+                {
+                    up(value);
+                }
+                if (direction == "down")
+                {
+                    down(value);
+                }
+                else
+                {
+                    throw new Exception("Sorry, directions unclear");
+                }
+            }
+
+            finalPosition(horizontalPosition, depth);
         }
 
+        public int forward(int x)
+        {
+            horizontalPosition += x;
+            return horizontalPosition;
+        }
 
+        public int up(int y)
+        {
+            return depth -= y;
+        }
+
+        public int down(int y)
+        {
+            return depth += y;
+        }
+
+        public int finalPosition(int x, int y)
+        {
+            return x * y;
+        }
         public decimal Answer_Part1 { get; private set; }
 
         public decimal Answer_Part2 { get; private set; }
