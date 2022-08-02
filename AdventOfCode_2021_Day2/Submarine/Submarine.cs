47e1f1e67214e140372abf9f1e4144eb707d9131
acheatham87 <acheatham87@gmail.com>
2022-08-02 10:54:16 -0500

    init commit

--- AdventOfCode_2021_Day2/Submarine/Submarine.cs
+++ AdventOfCode_2021_Day2/Submarine/Submarine.cs
@@ -4,58 +4,58 @@
 namespace Submarine {
     public class Submarine : ISubmarine 
     {
-        public int depth = 0;
-        public int horizontalPosition = 0;
+        //public int depth = 0;
+        //public int horizontalPosition = 0;
 
         public Submarine(string InstructionFile = "input.txt") : this(File.ReadAllLines(InstructionFile)) { }
 
         public Submarine(string[] Instructions) {
-            foreach (string[] instruction in Instructions)
-            {
-                instruction.Split(" ");
-                string direction = instruction[0];
-                int value = int.Parse(instruction[1]);
+        //    foreach (string[] instruction in Instructions)
+        //    {
+        //        instruction.Split(" ");
+        //        string direction = instruction[0];
+        //        int value = int.Parse(instruction[1]);
 
-                if(direction == "forward")
-                {
-                    forward(value);
-                }
-                if (direction == "up")
-                {
-                    up(value);
-                }
-                if (direction == "down")
-                {
-                    down(value);
-                }
-                else
-                {
-                    throw new Exception("Sorry, directions unclear");
-                }
-            }
+        //        if (direction == "forward")
+        //        {
+        //            forward(value);
+        //        }
+        //        if (direction == "up")
+        //        {
+        //            up(value);
+        //        }
+        //        if (direction == "down")
+        //        {
+        //            down(value);
+        //        }
+        //        else
+        //        {
+        //            throw new Exception("Sorry, directions unclear");
+        //        }
+        //    }
 
-            finalPosition(horizontalPosition, depth);
-        }
+        //    finalPosition(horizontalPosition, depth);
+        //}
 
-        public int forward(int x)
-        {
-            horizontalPosition += x;
-            return horizontalPosition;
-        }
+        //public int forward(int x)
+        //{
+        //    horizontalPosition += x;
+        //    return horizontalPosition;
+        //}
 
-        public int up(int y)
-        {
-            return depth -= y;
-        }
+        //public int up(int y)
+        //{
+        //    return depth -= y;
+        //}
 
-        public int down(int y)
-        {
-            return depth += y;
-        }
+        //public int down(int y)
+        //{
+        //    return depth += y;
+        //}
 
-        public int finalPosition(int x, int y)
-        {
-            return x * y;
+        //public int finalPosition(int x, int y)
+        //{
+        //    return x * y;
         }
         public decimal Answer_Part1 { get; private set; }
 
